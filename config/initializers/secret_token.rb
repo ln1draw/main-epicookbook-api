2e46bec7a823dade4383fa5f740ba1de59471bf9
shanghaiellen <shanghaiellen@gmail.com>
2014-04-03 09:59:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MainEpicookbookAPI::Application.config.secret_key_base = '47c10f12c728572b63c740f3e566a2edee667d5dfecfd968433553b41b851e742cb155d02a74374f2b693d64a54a9e98dba38aec889bd3f6c3ee7569a407e407'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MainEpicookbookAPI::Application.config.session_store :cookie_store, key: '_MainEpicookbookAPI_session'
